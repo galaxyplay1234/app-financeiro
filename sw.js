488d9871b206d2d1172ea2bc501d400a5533f38e
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-01-30 12:16:38 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = "financas-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/app.html",
-  "/relatorios.html",
-  "/manifest.json"
-];
-
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
