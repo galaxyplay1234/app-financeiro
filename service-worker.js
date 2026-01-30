b1663298af912e80eb995d9bfdb0f92037d4a059
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-01-30 12:15:32 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-k
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  // SEM CACHE — sempre busca da internet
+  event.respondWith(fetch(event.request));
+});
